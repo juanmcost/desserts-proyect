6c5d6fbb7abb98e8af86fa10f2fea4566e3568b9
juanmcost <jmcostanzo610@gmail.com>
2022-05-10 22:35:55 -0300

    creating models

--- dessertsBack/app/models.py
+++ dessertsBack/app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.value
+
+class Dessert(models.Model):
+    name = models.TextField(max_length=100)
+    picture = models.ImageField(upload_to ='uploads/desserts-pics/', max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    tags = models.ManyToManyField(Tag, related_name="desserts")
+
+
+    def __str__(self):
+        return self.name + " ($" + self.price +")"
+
+class Order(models.Model):
+    charge = models.DecimalField(max_digits=10, decimal_places=2)
+    desserts = models.ManyToManyField(Dessert, related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+    comments = models.TextField(max_length=500)
+
+    def save(self, *args, **kwargs):
+        self.charge = 0
+        for item in self.desserts:
+            self.charge = self.charge + item.price
+        super(Order, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.created_at + " $"+self.charge
