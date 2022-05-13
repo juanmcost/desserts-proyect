8116ae704e602b7d6c57c25a94e65a4ecc19d76d
juanmcost <jmcostanzo610@gmail.com>
2022-05-12 23:58:27 -0300

    chakra installed

--- desserts-front/pages/_app.tsx
+++ desserts-front/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../styles/theme'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ desserts-front/styles/theme.tsx
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const shadows = {
+  outline: "0 0 0 3px rgba(66, 153, 225, 0)",
+}
+
+const theme = extendTheme({ 
+  colors, 
+  config,
+  shadows,
+})
+
+export default theme
